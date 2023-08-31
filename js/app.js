38021a5d524435589e8a735ecd14013a76400515
Pis4ni <pis4wrk@gmail.com>
2023-08-31 17:14:37 +0200

    add: basic to do list logic

--- js/app.js
+++ js/app.js
@@ -3,7 +3,37 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
-      
+        // titolo
+        title: 'To Do List',
+        // to do list
+        list:[
+            {
+                text: 'fare la spesa',
+                done: false
+            },
+            {
+                text: 'fare la lavatrice',
+                done: false
+            },
+            {
+                text: 'fare la lavastoviglie',
+                done: false
+            },
+        ],
+        newTask:''
+
+
     }
-  }
+  },
+  methods:{
+    addNewTask(){
+        let taskObj = {
+            text:this.newTask,
+            done:false
+        }
+        this.list.push(taskObj)
+        this.newTask = ''
+        console.log(this.list);
+    }
+  },
 }).mount('#app')
